a82a89ce4d958128543c656d886308be6f9847a5
Viki <hi@viki.moe>
2023-01-06 12:29:33 +0800

    feat: add data

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
-import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import { serve } from 'https://deno.land/std@0.155.0/http/server.ts'
 
-serve((req: Request) => new Response("Hello World"));
+const files = [...Deno.readDirSync('./static')]
+
+async function handleRequest(request: Request): Promise<Response> {
+  const { pathname } = new URL(request.url)
+
+  for (const file of files) {
+    if (file.name === pathname.replace('/', '')) {
+      const file = await Deno.readFile(`./static${pathname}`)
+
+      return new Response(file, {
+        headers: {
+          'content-type': 'application/json; charset=utf-8'
+        }
+      })
+    }
+  }
+
+  return new Response('404', {
+    headers: {
+      'content-type': 'text/plain; charset=utf-8'
+    }
+  })
+}
+
+serve(handleRequest)
