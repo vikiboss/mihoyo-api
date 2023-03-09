ce78b468fdb4860ff32ba0351c5bdd7af9f54110
Viki <hi@viki.moe>
2023-03-09 12:45:42 +0800

    feat: add license & modify settings

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# API data for Genshin Helper（原神助手）
+# API for Genshin Helper
 
-Endpoints:
+Fast static data API for [Genshin Helper （原神助手）](https://github.com/vikiboss/gs-helper).
+
+## Endpoints
 
 - [roles.json](https://ys.viki.moe/roles.json)
 - [strategies.json](https://ys.viki.moe/strategies.json)
 - [portals.json](https://ys.viki.moe/portals.json)
 - [info.json](https://ys.viki.moe/info.json)
+
+## Thanks
+
+This API is hosted by [Deno Deploy](https://deno.dev).
+
+## License
+
+- [MIT](LICENSE) License © 2023-PRESENT Viki
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.155.0/http/server.ts'
+import { serve } from 'https://deno.land/std@0.178.0/http/server.ts'
 
 const files: Map<string, Uint8Array> = new Map()
 
@@ -7,23 +7,18 @@ for await (const { name } of Deno.readDir('./static')) {
   files.set(name, data)
 }
 
+const headers = { 'Content-Type': 'application/json; charset=utf-8' }
+const notFoundRes = JSON.stringify({ code: 404, msg: 'not found', data: '' })
+
 function handleRequest(request: Request): Response {
   const { pathname } = new URL(request.url)
   const name = pathname.replace('/', '')
 
   if (files.has(name)) {
-    return new Response(files.get(name), {
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-      }
-    })
+    return new Response(files.get(name), { headers })
   }
 
-  return new Response('404', {
-    headers: {
-      'Content-Type': 'text/plain; charset=utf-8'
-    }
-  })
+  return new Response(notFoundRes, { headers })
 }
 
 serve(handleRequest)
