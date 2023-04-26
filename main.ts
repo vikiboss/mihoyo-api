cb7b0826fe3bd0f58d3e20a2e9d2b5a78ec84f6a
Viki <hi@viki.moe>
2023-04-26 11:36:26 +0800

    fix: fix content type

--- main.ts
+++ main.ts
@@ -4,6 +4,11 @@ import router from './router.ts'
 const app = new oak.Application()
 
 app.use(async (ctx, next) => {
+  ctx.response.headers.set('content-type', 'application/json; charset=utf-8')
+  await next()
+})
+
+app.use(async (ctx, next) => {
   const isHome = ctx.request.url.pathname === '/'
 
   if (isHome) {
